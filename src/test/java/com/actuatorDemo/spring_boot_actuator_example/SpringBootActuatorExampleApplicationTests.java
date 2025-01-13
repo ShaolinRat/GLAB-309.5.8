5e2c6514bd83e24148434307f750636cf53c6522
ShaolinRat <csprowalps@gmail.com>
2025-01-13 12:27:29 -0500

    adding files

---
+++ src/test/java/com/actuatorDemo/spring_boot_actuator_example/SpringBootActuatorExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.actuatorDemo.spring_boot_actuator_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootActuatorExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
